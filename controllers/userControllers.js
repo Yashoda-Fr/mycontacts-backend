ecf886c80d658f8d982efdb0cab2319114bdf2de
yashoda-fr <fyashodha@gmail.com>
2024-02-03 10:56:52 +0530

    create token

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -47,7 +47,28 @@ const registerUser = asyncHandler(async (req,res)=> {
 //access Public
 
 const loginUser = asyncHandler(async (req,res)=>{
-    res.json({message:"login the user"});
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mendatory !");
+    }
+const user = await User.findOne({email});
+    //to compare the password with hash password
+    if (user && (await bcrypt.compare(/*entered password during the login attempt.*/password, /*hashed password stored in the user object retrieved from the database.*/user.password))) {
+       const accesstoken =jwt.sign({
+           user:{username: user.name,
+               email: user.email,
+               id: user._id,
+           },
+       },process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1m"});
+        res.status(200).json({accesstoken})
+
+    }
+    else {
+        res.status(400);
+        throw new Error("Email or password  is not valid !");
+    }
+
 });
 
 //@decs current user information
